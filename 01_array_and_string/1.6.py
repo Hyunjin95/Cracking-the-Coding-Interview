382f0ea4695774912e40269f3b0707c17ed905b3
Hyunjin95 <das01063@snu.ac.kr>
2019-02-07 18:17:27 +0900

    Add Problem 1.6

---
+++ 01_array_and_string/1.6.py
@@ -0,0 +1,36 @@
+import unittest
+
+
+# Compress a string by replacing consecutive characters with a character and a number.
+# Return the input string if the length of the compressed string is longer than the original string.
+def compress(string):
+    if len(string) == 0:
+        return string
+
+    count = 1
+    compressed = string[0]
+    for i in range(1, len(string), 1):
+        if string[i] == string[i-1]:
+            count += 1
+        else:
+            compressed += str(count)
+            compressed += string[i]
+            count = 1
+    compressed += str(count)
+
+    if len(compressed) > len(string):
+        return string
+    return compressed
+
+
+class Test(unittest.TestCase):
+    
+    def test(self):
+        self.assertEqual(compress("abcde"), "abcde")
+        self.assertEqual(compress("aaaaabbcde"), "a5b2c1d1e1")
+        self.assertEqual(compress("abbbcdee"), "abbbcdee")
+        self.assertEqual(compress("abbbbbcdee"), "a1b5c1d1e2")
+
+
+if __name__ == '__main__':
+    unittest.main()
