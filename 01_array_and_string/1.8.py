ae38b5b53bc63ae3dc6256db128cf4e63153646f
Hyunjin95 <das01063@snu.ac.kr>
2019-02-07 19:11:20 +0900

    Add Problem 1.8

---
+++ 01_array_and_string/1.8.py
@@ -0,0 +1,67 @@
+import unittest
+
+# If a number of a MxN matrix is 0, set all rows and columns that have the number to 0.
+# The limit of additional space is O(1)!
+def zero_matrix(mat):
+    if len(mat) == 0:
+        return
+    
+    len_row, len_col = len(mat), len(mat[0])
+    row_has_zero, col_has_zero = False, False
+    
+    for i in range(len_row):
+        if mat[i][0] == 0:
+            row_has_zero = True
+            break
+    
+    for j in range(len_col):
+        if mat[0][j] == 1:
+            col_has_zero = True
+            break
+    
+    for i in range(1, len_row, 1):
+        for j in range(1, len_col, 1):
+            if mat[i][j] == 0:
+                mat[i][0] = 0
+                mat[0][j] = 0
+    
+    for i in range(len_row):
+        if mat[i][0] == 0:
+            for j in range(len_col):
+                mat[i][j] = 0
+    
+    for j in range(len_col):
+        if mat[0][j] == 0:
+            for i in range(len_row):
+                mat[i][j] = 0
+    
+    if row_has_zero:
+        for i in range(len_row):
+            mat[i][0] = 0
+    
+    if col_has_zero:
+        for j in range(len_col):
+            mat[0][j] = 0
+
+
+class Test(unittest.TestCase):
+    
+    def test(self):
+        test_mat = [
+            [1, 2, 3, 0],
+            [5, 0, 7, 8],
+            [9, 10, 11, 12],
+            [0, 14, 15, 16]
+        ]
+        expected = [
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 11, 0],
+            [0, 0, 0, 0]
+        ]
+        zero_matrix(test_mat)
+        self.assertEqual(test_mat, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
