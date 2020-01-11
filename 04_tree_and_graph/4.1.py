ceb7351163e4bf67579bc58c7b285f421631a86b
Hyunjin95 <das01063@snu.ac.kr>
2020-01-11 15:07:28 +0900

    Add a solution using BFS

--- 04_tree_and_graph/4.1.py
+++ 04_tree_and_graph/4.1.py
@@ -14,7 +14,7 @@ def exists_path(c, a, b):
     for n in vertices:
         visited[n] = False
     visit(a, b, visited, graph)            
-    print(visited)
+    print("DFS -", visited)
 
     return visited[b]
 
@@ -33,9 +33,40 @@ def visit(a, b, visited, graph):
                 break
 
 
+# Use BFS
+def exists_path_bfs(c, a, b):
+    if a == b:
+        return True
+
+    graph, vertices = c.graph, c.vertices
+    # Act like a queue by using insert() and pop()
+    queue = []
+    visited = dict()
+
+    for n in vertices:
+        visited[n] = False
+    visited[a] = True
+    queue.insert(0, a)
+
+    while queue:
+        root = queue.pop()
+        root_node = graph[root]
+
+        if root is b:
+            break
+
+        for neighbor in root_node:
+            if not visited[neighbor]:
+                queue.insert(0, neighbor)
+                visited[neighbor] = True
+        
+    print("BFS -", visited)
+    return visited[b]
+
+
 class Test(unittest.TestCase):
 
-    def test(self):
+    def test_dfs(self):
         g = GraphUsingList()
         g.add_edge(0, 1, 5)
         g.add_edge(1, 2, 1)
@@ -65,6 +96,37 @@ class Test(unittest.TestCase):
         self.assertEqual(exists_path(g, 0, 9), True)
         self.assertEqual(exists_path(g, 0, 7), True)
         self.assertEqual(exists_path(g, 0, 1), True)
+    
+    def test_bfs(self):
+        g = GraphUsingList()
+        g.add_edge(0, 1, 5)
+        g.add_edge(1, 2, 1)
+        g.add_edge(2, 4, 3)
+        g.add_edge(4, 3, 2)
+
+        self.assertEqual(exists_path_bfs(g, 1, 3), True)
+        self.assertEqual(exists_path_bfs(g, 0, 3), True)
+        self.assertEqual(exists_path_bfs(g, 0, 2), True)
+        self.assertEqual(exists_path_bfs(g, 0, 4), True)
+        self.assertEqual(exists_path_bfs(g, 1, 0), False)
+        self.assertEqual(exists_path_bfs(g, 3, 4), False)
+
+        g.add_edge(6, 1, 3)
+        g.add_edge(1, 0, 2)
+        g.add_edge(6, 4, 2)
+
+        self.assertEqual(exists_path_bfs(g, 6, 0), True)
+        self.assertEqual(exists_path_bfs(g, 6, 3), True)
+        self.assertEqual(exists_path_bfs(g, 1, 0), True)
+        self.assertEqual(exists_path_bfs(g, 4, 6), False)
+
+        g.add_edge(1, 7, 2)
+        g.add_edge(7, 8, 2)
+        g.add_edge(8, 9, 2)
+
+        self.assertEqual(exists_path_bfs(g, 0, 9), True)
+        self.assertEqual(exists_path_bfs(g, 0, 7), True)
+        self.assertEqual(exists_path_bfs(g, 0, 1), True)
 
 
 
