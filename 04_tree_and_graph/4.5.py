51cffd36c138b64302dd1f15c1b53d3b0a1bc4b7
Hyunjin95 <das01063@snu.ac.kr>
2020-01-12 23:18:37 +0900

    Add a solution to 4.5

--- 04_tree_and_graph/4.5.py
+++ 04_tree_and_graph/4.5.py
@@ -1,1 +1,67 @@
-# BST 검증: 주어진 이진 트리가 이진 탐색 트리인지 확인하는 함수를 작성하라.
+import unittest
+from tree import Node, BinarySearchTree
+
+
+
+# Write a function that determines whether a given binary tree is a binary search tree.
+def check_bst(t):
+    node = t.root
+    if not node:
+        return False
+    
+    return _check_bst(node, -9999999, 9999999)
+
+
+def _check_bst(node, min_range, max_range):
+    if not node:
+        return True
+
+    value = node.item
+    left, right = node.left, node.right
+
+    if left and not _check_bst(left, min_range, value) or right and not _check_bst(right, value, max_range):
+            return False
+
+    if min_range < value <= max_range:
+        return True
+    return False
+
+
+class Test(unittest.TestCase):
+    
+    def test(self):
+        # Using the BST class, but it is not always a BST.
+        t = BinarySearchTree()
+        t.insert(0)
+        self.assertEqual(check_bst(t), True)
+        r = t.root
+        r.left = Node(1)
+        self.assertEqual(check_bst(t), False)
+        r.left = Node(0)
+        self.assertEqual(check_bst(t), True)
+        r.right = Node(0)
+        self.assertEqual(check_bst(t), False)
+        r.left = Node(-1)
+        r.right = Node(2)
+        self.assertEqual(check_bst(t), True)
+        r.left.left = Node(-3)
+        r.left.right = Node(-2)
+        self.assertEqual(check_bst(t), False)
+        r.left.right = Node(3)
+        self.assertEqual(check_bst(t), False)
+        r.left.right = Node(-0.5)
+        self.assertEqual(check_bst(t), True)
+        r.right.left = Node(-10)
+        self.assertEqual(check_bst(t), False)
+        r.right.left = Node(1.5)
+        self.assertEqual(check_bst(t), True)
+        r.right.right = Node(2)
+        self.assertEqual(check_bst(t), False)
+        r.right.right = Node(4)
+        self.assertEqual(check_bst(t), True)
+        r.left = Node(0)
+        self.assertEqual(check_bst(t), True)
+
+
+if __name__ == '__main__':
+    unittest.main()
