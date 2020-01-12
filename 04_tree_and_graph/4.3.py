3777eb27e4d292223dbea583eccde360ff960c00
Hyunjin95 <das01063@snu.ac.kr>
2020-01-12 16:17:53 +0900

    Solve Problem 4.3

--- 04_tree_and_graph/4.3.py
+++ 04_tree_and_graph/4.3.py
@@ -1,3 +1,54 @@
-# When a binary tree is given, make linked lists of the nodes at the same depth of the list.
-# If the depth is D, the number of linked lists is D.
+import unittest
+from tree import BinarySearchTree
 
+
+
+# When a binary tree is given, make lists of the nodes at the same depth of the tree.
+# If the depth is D, the number of the lists is D.
+def lists_of_tree(tree):
+    root = tree.root
+    lists_of_nodes = [[] for _ in range(tree.get_depth())]
+
+    q = []
+    q.insert(0, (root, 0))
+    while q:
+        curr = q.pop()
+        node, depth = curr[0], curr[1]
+        lists_of_nodes[depth].append(node.item)
+
+        if node.left:
+            q.insert(0, (node.left, depth+1))
+        if node.right:
+            q.insert(0, (node.right, depth+1))
+    
+    return lists_of_nodes
+
+
+class Test(unittest.TestCase):
+    
+    def test(self):
+        t = BinarySearchTree()
+        t.insert(5)
+        self.assertEqual(lists_of_tree(t), [[5]])
+        t.insert(3)
+        self.assertEqual(lists_of_tree(t), [[5], [3]])
+        t.insert(7)
+        self.assertEqual(lists_of_tree(t), [[5], [3, 7]])
+        t.insert(2)
+        self.assertEqual(lists_of_tree(t), [[5], [3, 7], [2]])
+        t.insert(4)
+        self.assertEqual(lists_of_tree(t), [[5], [3, 7], [2, 4]])
+        t.insert(6)
+        self.assertEqual(lists_of_tree(t), [[5], [3, 7], [2, 4, 6]])
+        t.insert(8)
+        self.assertEqual(lists_of_tree(t), [[5], [3, 7], [2, 4, 6, 8]])
+        t.insert(9)
+        self.assertEqual(lists_of_tree(t), [[5], [3, 7], [2, 4, 6, 8], [9]])
+        t.insert(10)
+        self.assertEqual(lists_of_tree(t), [[5], [3, 7], [2, 4, 6, 8], [9], [10]])
+        t.insert(0)
+        self.assertEqual(lists_of_tree(t), [[5], [3, 7], [2, 4, 6, 8], [0, 9], [10]])
+
+
+if __name__ == '__main__':
+    unittest.main()
--- 04_tree_and_graph/tree.py
+++ 04_tree_and_graph/tree.py
@@ -34,10 +34,102 @@ class Node:
         self._right = right
 
 
-class BinaryTree:
-    """ A binary tree """
+class BinarySearchTree:
+    """ A binary search tree """
     def __init__(self):
         self._root = None
+
+    def __str__(self):
+        return self.print_inorder(self.root)
+
+    @property
+    def root(self):
+        return self._root
+    
+    @root.setter
+    def root(self, root):
+        self._root = root
     
     def insert(self, item):
-        
+        node = Node(item)
+        if self.is_empty():
+            self.root = node
+        else:
+            curr = self.root
+            while curr:
+                if curr.item >= item:
+                    if curr.left:
+                        curr = curr.left
+                    else:
+                        curr.left = node
+                        break
+                else:
+                    if curr.right:
+                        curr = curr.right
+                    else:
+                        curr.right = node
+                        break
+    
+    def search(self, item):
+        node = self.root
+        while node:
+            if node.item == item:
+                return True
+            elif node.item < item:
+                node = node.right
+            else:
+                node = node.left
+        return False
+    
+    def print_inorder(self, n):
+        if not n:
+            return ""
+        if not (n.left or n.right):
+            return str(n.item)
+
+        return self.print_inorder(n.left) + " -> " + str(n.item) + " -> " + self.print_inorder(n.right)
+    
+    def is_empty(self):
+        if not self.root:
+            return True
+        return False
+    
+    def get_depth(self):
+        if self.is_empty():
+            return 0
+        return self._get_depth(self.root)
+    
+    def _get_depth(self, n):
+        if not n:
+            return 0
+        return max(self._get_depth(n.left), self._get_depth(n.right)) + 1          
+
+
+class Test(unittest.TestCase):
+
+    def test_bst(self):
+        bst = BinarySearchTree()
+        bst.insert(3)
+        self.assertEqual(bst.get_depth(), 1)
+        bst.insert(4)
+        self.assertEqual(bst.get_depth(), 2)
+        bst.insert(2)
+        self.assertEqual(bst.get_depth(), 2)
+        bst.insert(0)
+        self.assertEqual(bst.get_depth(), 3)
+        bst.insert(6)
+        self.assertEqual(bst.get_depth(), 3)
+        bst.insert(1)
+        self.assertEqual(bst.get_depth(), 4)
+        print(bst)
+        self.assertTrue(bst.search(0))
+        self.assertTrue(bst.search(1))
+        self.assertTrue(bst.search(2))
+        self.assertTrue(bst.search(3))
+        self.assertTrue(bst.search(4))
+        self.assertTrue(bst.search(6))
+        self.assertFalse(bst.search(5))
+
+
+if __name__ == '__main__':
+    unittest.main()
