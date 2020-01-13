db90cd9eb30420477a9cfff2a2b1eb9b8efb7d4a
Hyunjin95 <das01063@snu.ac.kr>
2020-01-13 18:01:20 +0900

    Add a solution to 4.7 by using topological sort
    Needed to handle when there is a cycle

--- 04_tree_and_graph/4.7.py
+++ 04_tree_and_graph/4.7.py
@@ -1,6 +1,94 @@
-# 순서 정하기: 프로젝트의 리스트와 프로젝트들 간의 종속 관계 가 주어졌을 때, 
-# 프로젝트를 수행해 나가는 순서를 찾으라. 유효한 순서가 존재하지 않으면 에러를 반환한다.
-# 종속 관계란 - 프로젝트 쌍이 리스트로 주어지면 각 프로젝트 쌍에서 두 번째 프로젝트가 첫 번째 프로젝트에 종속되어 있다는 뜻
-# 입력: 프로젝트: a, b, c, d, e, f
-#       종속 관계: (a, d), (f, b), (b, d), (f, a), (d, c)
-# 출력: f, e, a, b, d, c
+import unittest
+from collections import defaultdict
+from graph import GraphUsingList
+
+
+
+# When a list of projects and their dependencies are given, find an order of the projects.
+# If there is no valid order, return error.
+# Dependency - In (a, b), b is dependent on a.
+# Example -
+# Input:    projects: a, b, c, d, e, f
+#           dependencies: (a, d), (f, b), (b, d), (f, a), (d, c)
+# Output: f, e, a, b, d, c
+def find_order(projects, dependencies):
+    edges = build_graph(projects, dependencies)
+
+    path = topological_sort(projects, edges)
+    if path:
+        print("path:", path)
+        return path
+    
+    raise RuntimeError("There is no valid order.")
+        
+
+def build_graph(vertices, edges):
+    # Both two dicts are the same dictionaries. g uses the defaultdict class in the collections module.
+    g, dict_practice = defaultdict(dict), {}
+    depending_vertices = set()
+
+    # Initialize the dictionaries
+    for vertex in vertices:
+        g[vertex]
+        dict_practice[vertex] = {}
+
+    # Add edges to the dictionaries
+    for start, end in edges:
+        g[start][end] = 1
+        dict_practice[start].update({end: 1})
+        depending_vertices.add(end)
+
+    return g
+
+
+def topological_sort(vertices, edges):
+    visited = initialize_visited(vertices)
+    topological_order = []
+
+    for v in vertices:
+        if not visited[v]:
+            _topological_sort(v, edges, visited, topological_order)
+
+    return topological_order
+
+
+def _topological_sort(vertex, edges, visited, topological_order):
+    visited[vertex] = True
+
+    for neighbor in edges[vertex]:
+        if not visited[neighbor]:
+            _topological_sort(neighbor, edges, visited, topological_order)
+
+    topological_order.insert(0, vertex)
+
+
+def initialize_visited(vertices):
+    visited = {}
+    for v in vertices:
+        visited[v] = False
+    return visited
+
+
+class Test(unittest.TestCase):
+
+    def test(self):
+        projects = {"a", "b", "c", "d", "e", "f"}
+        dependencies = {("a", "d"), ("f", "b"), ("b", "d"), ("f", "a"), ("d", "c")}
+        self.assertIsNotNone(find_order(projects, dependencies))
+
+        projects = {"a"}
+        dependencies = {}
+        self.assertIsNotNone(find_order(projects, dependencies))
+
+        projects = {"a", "b"}
+        dependencies = {}
+        self.assertIsNotNone(find_order(projects, dependencies))
+
+        projects = {"a", "b", "c"}
+        dependencies = {("a", "c")}
+        with self.assertRaises(RuntimeError):
+            find_order(projects, dependencies)
+
+
+if __name__ == "__main__":
+    unittest.main()
