6cf03f7ac6cc66850de4e486fb38a94c52fb9d7f
Hyunjin95 <das01063@snu.ac.kr>
2020-01-14 17:37:44 +0900

    Add a solution to 4.8

--- 04_tree_and_graph/4.8.py
+++ 04_tree_and_graph/4.8.py
@@ -1,2 +1,55 @@
-# 첫 번째 공통 조상: 이진 트리에서 노드 두 개가 주어졌을 때, 이 두 노드의 첫 번째 공통 조상을 찾는 알고리즘을 설계하고 그 코드를 작성하라.
-# 자료구조 내에 추가로 노드를 저장해 두면 안 된다. 반드시 이진 탐색 트리일 필요는 없다.
+import unittest
+from tree import Node, BinarySearchTree
+
+
+
+# When two nodes of a binary tree are given, find the first common ancestor of the nodes.
+# Restriction: You cannot save any other node in your data structure. The tree doesn't have to be a BST.
+def first_common_ancestor(t, a, b):
+    node = t.root
+    if not (find(node, a) and find(node, b)):
+        return None
+
+    return _first_common_ancestor(node, a, b)
+        
+
+def _first_common_ancestor(root, a, b):
+    if root is None or root is a or root is b:
+        return root
+
+    a_is_left, b_is_left = find(root.left, a), find(root.left, b)
+    if a_is_left != b_is_left:
+        return root
+    
+    child = root.left if a_is_left else root.right
+    return _first_common_ancestor(child, a, b)
+
+
+def find(root, dest):
+    if root is None:
+        return False
+    if root is dest:
+        return True
+    
+    return find(root.left, dest) or find(root.right, dest)
+
+
+class Test(unittest.TestCase):
+
+    def test(self):
+        t = BinarySearchTree()
+        t.insert(1)
+        root = t.root
+        root.left = Node(2)
+        root.right = Node(3)
+        self.assertEqual(first_common_ancestor(t, root.left, root.right), root)
+        self.assertIsNone(first_common_ancestor(t, root.left, Node(4)), None)
+        root.left.left = Node(1)
+        root.left.right = Node(2)
+        self.assertEqual(first_common_ancestor(t, root.left.left, root.left.right), root.left)
+        self.assertEqual(first_common_ancestor(t, root.left.left, root), root)
+        self.assertEqual(first_common_ancestor(t, root.left.left, root.right), root)
+
+
+if __name__ == "__main__":
+    unittest.main()
