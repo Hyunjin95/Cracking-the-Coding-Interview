4cdc9c2b62379938ecdcfa834c6988cddec6d502
Hyunjin95 <das01063@snu.ac.kr>
2020-01-14 16:39:24 +0900

    Add a solution to 4.7
    Used topological sorting algorithm

--- 04_tree_and_graph/4.7.py
+++ 04_tree_and_graph/4.7.py
@@ -1,5 +1,6 @@
 import unittest
 from collections import defaultdict
+from queue import Queue
 from graph import GraphUsingList
 
 
@@ -12,10 +13,11 @@ from graph import GraphUsingList
 #           dependencies: (a, d), (f, b), (b, d), (f, a), (d, c)
 # Output: f, e, a, b, d, c
 def find_order(projects, dependencies):
-    edges = build_graph(projects, dependencies)
+    g = build_graph(projects, dependencies)
+    edges, list_num_lines = g[0], g[1]
 
-    path = topological_sort(projects, edges)
-    if path:
+    path = topological_sort(projects, edges, list_num_lines)
+    if path and len(path) == len(projects):
         print("path:", path)
         return path
     
@@ -23,50 +25,43 @@ def find_order(projects, dependencies):
         
 
 def build_graph(vertices, edges):
-    # Both two dicts are the same dictionaries. g uses the defaultdict class in the collections module.
+    # Both two dicts has the same items. g uses the defaultdict class in the collections module.
     g, dict_practice = defaultdict(dict), {}
-    depending_vertices = set()
+    list_num_lines = {}
 
     # Initialize the dictionaries
     for vertex in vertices:
         g[vertex]
         dict_practice[vertex] = {}
+        list_num_lines[vertex] = 0
 
     # Add edges to the dictionaries
     for start, end in edges:
         g[start][end] = 1
         dict_practice[start].update({end: 1})
-        depending_vertices.add(end)
+        list_num_lines[end] += 1
 
-    return g
-
-
-def topological_sort(vertices, edges):
-    visited = initialize_visited(vertices)
-    topological_order = []
-
-    for v in vertices:
-        if not visited[v]:
-            _topological_sort(v, edges, visited, topological_order)
-
-    return topological_order
+    return (g, list_num_lines)
 
 
-def _topological_sort(vertex, edges, visited, topological_order):
-    visited[vertex] = True
+def topological_sort(vertices, edges, list_num_lines):
+    q = Queue()
+    list_order = []
 
-    for neighbor in edges[vertex]:
-        if not visited[neighbor]:
-            _topological_sort(neighbor, edges, visited, topological_order)
+    for v in list_num_lines:
+        if list_num_lines[v] == 0:
+            q.put(v)
+    
+    while not q.empty():
+        curr = q.get()
+        list_order.append(curr)
 
-    topological_order.insert(0, vertex)
+        for neighbor in edges[curr]:
+            list_num_lines[neighbor] -= 1
+            if list_num_lines[neighbor] == 0:
+                q.put(neighbor)
 
-
-def initialize_visited(vertices):
-    visited = {}
-    for v in vertices:
-        visited[v] = False
-    return visited
+    return list_order
 
 
 class Test(unittest.TestCase):
@@ -86,6 +81,20 @@ class Test(unittest.TestCase):
 
         projects = {"a", "b", "c"}
         dependencies = {("a", "c")}
+        self.assertIsNotNone(find_order(projects, dependencies))
+
+        projects = {"a", "b", "c"}
+        dependencies = {("a", "c"), ("a", "b"), ("c", "a")}
+        with self.assertRaises(RuntimeError):
+            find_order(projects, dependencies)
+
+        projects = {"a", "b", "c"}
+        dependencies = {("b", "c"), ("c", "b")}
+        with self.assertRaises(RuntimeError):
+            find_order(projects, dependencies)
+        
+        projects = {"a", "b", "c"}
+        dependencies = {("a", "b"), ("b", "c"), ("c", "b")}
         with self.assertRaises(RuntimeError):
             find_order(projects, dependencies)
 
