d616c77bb6475c91bc85ffe4cf8403a1f32c369a
Hyunjin95 <das01063@snu.ac.kr>
2020-01-15 16:10:24 +0900

    Add a solution to 4.9
    I could not solve this problem. The source code was translated into Python 3 from the answer in the book.

--- 04_tree_and_graph/4.9.py
+++ 04_tree_and_graph/4.9.py
@@ -1,3 +1,63 @@
-# BST 수열: 배열의 원소를 왼쪽에서부터 차례로 트리에 삽입함으로써 이진 탐색 트리를 생성할 수 있다.
-# 이진 탐색 트리 안에서 원소가 중복되지 않는다고 할 때, 해당 트리를 만들어 낼 수 있는 모든 가능한 배열을 출력하라.
-# 입력: 이진 탐색 트리 / 출력: 가능한 배열들
+import unittest
+from tree import BinarySearchTree
+
+
+# We can build a binary search tree by inserting items in an array one by one from the left.
+# If there are no duplicate items in the BST, print all possible arrays able to make that tree.
+# Input: a binary search tree / Output: possible arrays
+def arrays_making_bst(root):
+    result = []
+
+    if not root:
+        result.append([])
+        return result
+
+    first = []
+    first.append(root.item)
+
+    arrays_left = arrays_making_bst(root.left)
+    arrays_right = arrays_making_bst(root.right)
+
+    for left in arrays_left:
+        for right in arrays_right:
+            mixed_list = []
+            mix_left_and_right(left, right, first, mixed_list)
+            result.extend(mixed_list)
+
+    return result
+    
+
+def mix_left_and_right(left, right, temp_result, list_result):
+    # The order of items in each list should be kept the same.
+    if len(left) == 0 or len(right) == 0:
+        result = temp_result[:]
+        result.extend(left)
+        result.extend(right)
+        list_result.append(result)
+        return
+
+    head_left = left.pop(0)
+    temp_result.append(head_left)
+    mix_left_and_right(left, right, temp_result, list_result)
+    left.insert(0, head_left)
+    temp_result.pop()
+
+    head_right = right.pop(0)
+    temp_result.append(head_right)
+    mix_left_and_right(left, right, temp_result, list_result)
+    right.insert(0, head_right)
+    temp_result.pop()
+
+
+class Test(unittest.TestCase):
+
+    def test(self):
+        t = BinarySearchTree()
+        t.insert(2)
+        t.insert(1)
+        t.insert(3)
+        self.assertEqual(arrays_making_bst(t.root), [[2, 1, 3], [2, 3, 1]])
+
+
+if __name__ == "__main__":
+    unittest.main()
