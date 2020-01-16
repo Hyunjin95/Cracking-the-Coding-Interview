ffe190b069f3054f789a9f9a1867a8e29217a693
Hyunjin95 <das01063@snu.ac.kr>
2020-01-16 16:10:49 +0900

    Modify the comments

--- 04_tree_and_graph/4.12.py
+++ 04_tree_and_graph/4.12.py
@@ -5,8 +5,8 @@ from tree import BinarySearchTree
 
 # There is a binary tree where the value of each node is an integer (positive or negative).
 # We want to count the number of routes that the sum of integers is a specific number.
-# Routes do not need to traverse from the root to the leaf nodes, however, it should go downwards.
-# That is, we can only move from the parent nodes to the child nodes.
+# Routes do not have to start from the root and end at the leaf nodes, however, it should go downwards.
+# That is, you can only move from the parent nodes to the child nodes.
 def find_route():
     pass
 
