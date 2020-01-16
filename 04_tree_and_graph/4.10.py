3da06963a5d5a442d15963b70105f3ccba84edd7
Hyunjin95 <das01063@snu.ac.kr>
2020-01-16 12:42:56 +0900

    Make the solution more efficient

--- 04_tree_and_graph/4.10.py
+++ 04_tree_and_graph/4.10.py
@@ -6,18 +6,18 @@ from tree import Node, BinarySearchTree
 # There are two big binary trees T1 and T2.
 # Assuming T1 is much bigger than T2, check whether T2 is a subtree of T1.
 # If the subtree of a node N in T1 is the same as T2, T2 is a subtree of T1.
-def check_subtree(node, t2):
-    if t2 is None:
+def check_subtree(r1, r2):
+    if r2 is None:
         return True
         
-    if node is None:
+    if r1 is None:
         return False
 
-    if preorder_traversal(node) == preorder_traversal(t2.root):
+    if preorder_traversal(r1) == preorder_traversal(r2):
         return True
     
-    is_left_same = check_subtree(node.left, t2)
-    is_right_same = check_subtree(node.right, t2)
+    is_left_same = check_subtree(r1.left, r2)
+    is_right_same = check_subtree(r1.right, r2)
     return is_left_same or is_right_same
 
 
@@ -29,20 +29,32 @@ def preorder_traversal(root):
     return [root.item] + preorder_traversal(root.left) + preorder_traversal(root.right)
 
 
-# This function does not traverse until the node is the same as T2.root.
-def check_subtree_advanced(node, t2):
-    if t2 is None:
+# This function only traverses when the node contains the same item as r2.
+def check_subtree_efficient(r1, r2):
+    if r2 is None:
         return True
-    
-    if node is None:
+
+    if r1 is None:
         return False
 
-    if node is t2.root:
-        if preorder_traversal(node) == preorder_traversal(t2.root):
-            return True
+    if r1.item == r2.item and same_trees(r1, r2):
+        return True
     
-    return check_subtree_advanced(node.left, t2) or check_subtree_advanced(node.right, t2)
+    return check_subtree_efficient(r1.left, r2) or check_subtree_efficient(r1.right, r2)
 
+
+def same_trees(r1, r2):
+    if r1 is None and r2 is None:
+        return True
+
+    if r1 is None or r2 is None:
+        return False
+    
+    if r1.item != r2.item:
+        return False
+    
+    return same_trees(r1.left, r2.left) and same_trees(r1.right, r2.right)
+    
 
 class Test(unittest.TestCase):
 
@@ -61,18 +73,18 @@ class Test(unittest.TestCase):
         t2.insert(4)
         t2.insert(7)
         t2.insert(8)
-        self.assertEqual(check_subtree(t1.root, t2), False)
-        self.assertEqual(check_subtree_advanced(t1.root, t2), False)
+        self.assertEqual(check_subtree(t1.root, t2.root), False)
+        self.assertEqual(check_subtree_efficient(t1.root, t2.root), False)
         r.left.left.right = t2.root
         # Now t2 is a subtree of t1.
-        self.assertEqual(check_subtree(t1.root, t2), True)
-        self.assertEqual(check_subtree_advanced(t1.root, t2), True)
-        self.assertEqual(check_subtree(t1.root.left.left, t2), True)
-        self.assertEqual(check_subtree_advanced(t1.root.left.left, t2), True)
+        self.assertEqual(check_subtree(t1.root, t2.root), True)
+        self.assertEqual(check_subtree_efficient(t1.root, t2.root), True)
+        self.assertEqual(check_subtree(t1.root.left.left, t2.root), True)
+        self.assertEqual(check_subtree_efficient(t1.root.left.left, t2.root), True)
         r.left.left.right = Node(10)
         r.right.left.right = t2.root
-        self.assertEqual(check_subtree(t1.root, t2), True)
-        self.assertEqual(check_subtree_advanced(t1.root, t2), True)
+        self.assertEqual(check_subtree(t1.root, t2.root), True)
+        self.assertEqual(check_subtree_efficient(t1.root, t2.root), True)
 
 
 if __name__ == "__main__":
