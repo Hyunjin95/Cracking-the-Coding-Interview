63745c5920dac48f0e59fee8d182198239f9ad6b
Hyunjin95 <das01063@snu.ac.kr>
2020-01-16 17:45:19 +0900

    Add a more efficient solution to 4.12
    The efficient version of the solutions was just written exactly the same as the answer of the book.

--- 04_tree_and_graph/4.12.py
+++ 04_tree_and_graph/4.12.py
@@ -31,9 +31,51 @@ def count_paths_from(node, target_num, partial_sum):
     return num_paths
 
 
+def count_paths_efficient(root, target_num):
+    return _count_paths_efficient_helper(root, target_num, 0, {})
+
+
+def _count_paths_efficient_helper(node, target_num, partial_sum, dict_partial_sum):
+    if not node:
+        return 0
+    
+    partial_sum += node.item
+    current_sum = partial_sum - target_num
+    count_paths = dict_partial_sum.get(current_sum, 0)
+
+    if partial_sum == target_num:
+        count_paths += 1
+    
+    add_values(dict_partial_sum, current_sum, 1)
+    count_paths += _count_paths_efficient_helper(node.left, target_num, partial_sum, dict_partial_sum)
+    count_paths += _count_paths_efficient_helper(node.right, target_num, partial_sum, dict_partial_sum)
+    add_values(dict_partial_sum, current_sum, -1)
+    
+    return count_paths
+    
+
+def add_values(dict_partial_sum, k, v):
+    cnt = dict_partial_sum.get(k, 0) + v
+    if cnt == 0:
+        dict_partial_sum.pop(k)
+    else:
+        dict_partial_sum[k] = v
+
+
 
 class Test(unittest.TestCase):
+    
     def test(self):
+        t = BinarySearchTree()
+        t.insert(0)
+        t.insert(0)
+        t.insert(0)
+        self.assertEqual(count_paths(t.root, 0), 6)
+        t.root.item = 1
+        self.assertEqual(count_paths(t.root, 1), 3)
+        self.assertEqual(count_paths(t.root, 0), 3)
+
+    def test_efficient(self):
         t = BinarySearchTree()
         t.insert(0)
         t.insert(0)
