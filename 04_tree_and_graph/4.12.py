00fd544f95bce919b1848bb047267541a8c209ba
Hyunjin95 <das01063@snu.ac.kr>
2020-01-16 16:05:48 +0900

    Add a skeleton of 4.12

--- 04_tree_and_graph/4.12.py
+++ 04_tree_and_graph/4.12.py
@@ -1,3 +1,20 @@
-# 합의 경로: 각 노드의 값이 정수(음수 및 양수)인 이진 트리가 있다. 이때 정수의 합이 특정 값이 되도록 하는 경로의 개수를 세려고 한다.
-# 경로가 꼭 루트에서 시작해서 말단 노드에서 끝날 필요는 없지만 반드시 아래로 내려가야 한다.
-# 즉 부모 노드에서 자식 노드로만 움직일 수 있다. 알고리즘을 어떻게 설계할 것인가?
+import unittest
+from tree import BinarySearchTree
+
+
+
+# There is a binary tree where the value of each node is an integer (positive or negative).
+# We want to count the number of routes that the sum of integers is a specific number.
+# Routes do not need to traverse from the root to the leaf nodes, however, it should go downwards.
+# That is, we can only move from the parent nodes to the child nodes.
+def find_route():
+    pass
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
